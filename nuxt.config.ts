968d424ed8f186d89944a0dfb08deb59208d7d2d
farruh <farruh020202@gmail.com>
2024-11-05 14:11:53 +0500

    add meta tags and site title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,73 @@ export default defineNuxtConfig({
   },
   app: {
     baseURL: "/",
+
+    head: {
+      title: "Бесплатные объявления - Покупка, продажа, аренда товаров и услуг",
+      meta: [
+        {
+          hid: "description",
+          name: "description",
+          content:
+            "На нашем сайте вы найдете тысячи объявлений о покупке, продаже и аренде товаров и услуг по всей стране. Удобный поиск, актуальные предложения и возможность легко подать свое объявление!",
+        },
+        {
+          hid: "keywords",
+          name: "keywords",
+          content:
+            "объявления, продажа, покупка, аренда, услуги, товары, автомобили, недвижимость, работа, электроника, мода, детские товары",
+        },
+        {
+          hid: "og:title",
+          property: "og:title",
+          content:
+            "Бесплатные объявления - Покупка, продажа, аренда товаров и услуг",
+        },
+        {
+          hid: "og:description",
+          property: "og:description",
+          content:
+            "На нашем сайте вы найдете тысячи объявлений о покупке, продаже и аренде товаров и услуг по всей стране. Удобный поиск, актуальные предложения и возможность легко подать свое объявление!",
+        },
+        {
+          hid: "og:type",
+          property: "og:type",
+          content: "website",
+        },
+        {
+          hid: "twitter:card",
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          hid: "twitter:title",
+          name: "twitter:title",
+          content:
+            "Бесплатные объявления - Покупка, продажа, аренда товаров и услуг",
+        },
+        {
+          hid: "twitter:description",
+          name: "twitter:description",
+          content:
+            "На нашем сайте вы найдете тысячи объявлений о покупке, продаже и аренде товаров и услуг по всей стране. Удобный поиск, актуальные предложения и возможность легко подать свое объявление!",
+        },
+        {
+          hid: "twitter:image",
+          name: "twitter:image",
+          content: "/default-main-image.jpg",
+        },
+        {
+          hid: "robots",
+          name: "robots",
+          content: "index, follow",
+        },
+        {
+          hid: "author",
+          name: "author",
+          content: "77.uz",
+        },
+      ],
+    },
   },
   ssr: false,
 });
